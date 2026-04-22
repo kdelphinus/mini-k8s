79b3ec39de6bda3dd538251b77b4d10f00860862
kdelphinus <kmj951015@gmail.com>
2026-04-22 20:36:50 +0900

    🏗️ Implement JSON persistence and name validation in registry

--- pkg/registry/registry.go
+++ pkg/registry/registry.go
@@ -1,28 +1,62 @@
 package registry
 
 import (
+	"encoding/json"
 	"fmt"
 	"mini-k8s/pkg/resources"
+	"os"
+	"sync"
 )
 
 type Registry struct {
-	// Resources 인터페이스를 지킨 모든 것을 담음
+	mu        sync.RWMutex
 	resources map[string]resources.Resource
+	filePath  string // 저장할 파일 경로
 }
 
-// Registry의 생성자
-func NewRegistry() *Registry {
-	return &Registry{
+// NewRegistry Registry의 생성자
+func NewRegistry(path string) *Registry {
+	reg := &Registry{
 		resources: make(map[string]resources.Resource),
+		filePath:  path,
 	}
+	reg.load() // 기존 데이터 불러오기
+	return reg
 }
 
-// 등록(Create/Update)
-func (r *Registry) Register(res resources.Resource) {
+// save 파일에 저장하는 내부 메서드
+func (r *Registry) save() error {
+	data, err := json.MarshalIndent(r.resources, "", "	")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(r.filePath, data, 0644)
+}
+
+// load 파일에서 불어오는 내부 메서드
+func (r *Registry) load() {
+	data, err := os.ReadFile(r.filePath)
+	if err != nil {
+		return // 파일 없으면 빈 상태로 시작
+	}
+	// TODO 인터페이스를 바로 Unmarshal 하는 것은 복잡하므로 로직 보강 필요
+	json.Unmarshal(data, &r.resources)
+}
+
+// Register 등록(Create/Update)
+func (r *Registry) Register(res resources.Resource) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, exists := r.resources[res.GetName()]; exists {
+		return fmt.Errorf("resource %s already exists", res.GetName())
+	}
+
 	r.resources[res.GetName()] = res
+	return r.save() // 등록할 때마다 파일에 저장
 }
 
-// 단일 조회(Read)
+// Get 단일 조회(Read)
 func (r *Registry) Get(name string) (resources.Resource, error) {
 	res, ok := r.resources[name] // 'comma ok' 패턴
 	if !ok {
@@ -31,7 +65,7 @@ func (r *Registry) Get(name string) (resources.Resource, error) {
 	return res, nil
 }
 
-// 전체 목록 조회(List)
+// List 전체 목록 조회(List)
 func (r *Registry) List() []resources.Resource {
 	list := make([]resources.Resource, 0, len(r.resources))
 	for _, v := range r.resources {
@@ -40,7 +74,7 @@ func (r *Registry) List() []resources.Resource {
 	return list
 }
 
-// 삭제(Delete)
+// Delete 삭제(Delete)
 func (r *Registry) Delete(name string) {
 	delete(r.resources, name)
 }
